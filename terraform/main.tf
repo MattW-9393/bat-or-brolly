e1ba50326bb5de12b8658bd942f083cb34c1461c
MattW-9393 <matt.warne22@gmail.com>
2026-04-28 23:20:22 +0100

    Format terraform files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,34 +1,34 @@
 terraform {
-    required_providers {
-        aws = {
-            source  = "hashicorp/aws"
-            version = "~> 5.0"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
+  }
 
-    backend "s3" {
-        bucket         = "bat-or-brolly-terraform-state"
-        key            = "bat-or-brolly/terraform.tfstate"
-        region         = "eu-west-2"
-        dynamodb_table = "bat-or-brolly-terraform-locks"
-        encrypt        = true
-    }
+  backend "s3" {
+    bucket         = "bat-or-brolly-terraform-state"
+    key            = "bat-or-brolly/terraform.tfstate"
+    region         = "eu-west-2"
+    dynamodb_table = "bat-or-brolly-terraform-locks"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
-    region = var.region
+  region = var.region
 }
 
 # Data Source = data.type.name.attribute
 
 # VPC
 data "aws_vpc" "default" {
-    default = true
+  default = true
 }
 
 # Subnets
-data "aws_subnets" "default"{
-      filter {
+data "aws_subnets" "default" {
+  filter {
     name   = "vpc-id"
     values = [data.aws_vpc.default.id]
   }
@@ -38,268 +38,268 @@ data "aws_subnets" "default"{
 
 # ECR
 resource "aws_ecr_repository" "app_container" {
-    name = "${var.app_name}-${var.environment}"
-    image_tag_mutability = "IMMUTABLE"
+  name                 = "${var.app_name}-${var.environment}"
+  image_tag_mutability = "IMMUTABLE"
 
-    image_scanning_configuration {
-        scan_on_push = true
-    }
+  image_scanning_configuration {
+    scan_on_push = true
+  }
 
-    tags = {
-        Name = "${var.app_name}-${var.environment}"
-        Environment = var.environment
-        ManagedBy = "terraform"
-    }
+  tags = {
+    Name        = "${var.app_name}-${var.environment}"
+    Environment = var.environment
+    ManagedBy   = "terraform"
+  }
 }
 
 # CloudWatch Logs
 resource "aws_cloudwatch_log_group" "logs" {
-    # /ecs/app-name-environment
-    name = "/ecs/${var.app_name}-${var.environment}"
-    retention_in_days = 7
+  # /ecs/app-name-environment
+  name              = "/ecs/${var.app_name}-${var.environment}"
+  retention_in_days = 7
 
-    tags = {
-        Name = "${var.app_name}-${var.environment}"
-        Environment = var.environment
-        ManagedBy = "terraform"
-    }
+  tags = {
+    Name        = "${var.app_name}-${var.environment}"
+    Environment = var.environment
+    ManagedBy   = "terraform"
+  }
 }
 
 # IAM
 
 # Which role to Assume
 data "aws_iam_policy_document" "assume_role" {
-    statement {
-        effect  = "Allow"
-        actions = ["sts:AssumeRole"]
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
 
-        principals {
-            type        = "Service"
-            identifiers = ["ecs-tasks.amazonaws.com"]
-        }
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
     }
+  }
 }
 
 #Role
 resource "aws_iam_role" "role" {
-  name = "${var.app_name}-${var.environment}-execution-role"
+  name               = "${var.app_name}-${var.environment}-execution-role"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 
-    tags = {
-        Name        = "${var.app_name}-${var.environment}"
-        Environment = var.environment
-        ManagedBy   = "terraform"
-    }
+  tags = {
+    Name        = "${var.app_name}-${var.environment}"
+    Environment = var.environment
+    ManagedBy   = "terraform"
+  }
 
 }
 
 # Policy
 
 resource "aws_iam_role_policy_attachment" "policy" {
-    # role comes from the resource defined above
-    role       = aws_iam_role.role.name
-    policy_arn = "arn:aws:iam::aws:policy/AmazonECSTaskExecutionRolePolicy"
+  # role comes from the resource defined above
+  role       = aws_iam_role.role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonECSTaskExecutionRolePolicy"
 }
 
 # Security Groups
 
 # ALB Security Group
 resource "aws_security_group" "alb_sg" {
-    name        = "${var.app_name}-${var.environment}-alb-sg"
-    description = "Allow HTTP traffic to ALB"
-    vpc_id      = data.aws_vpc.default.id
+  name        = "${var.app_name}-${var.environment}-alb-sg"
+  description = "Allow HTTP traffic to ALB"
+  vpc_id      = data.aws_vpc.default.id
 
-ingress {
+  ingress {
     description = "HTTP from internet"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-}
+  }
 
-ingress {
+  ingress {
     description = "HTTPS from internet"
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-}
+  }
 
-egress {
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    tags = {
-        Name        = "${var.app_name}-${var.environment}"
-        Environment = var.environment
-        ManagedBy   = "terraform"
-    }
+  tags = {
+    Name        = "${var.app_name}-${var.environment}"
+    Environment = var.environment
+    ManagedBy   = "terraform"
+  }
 }
 
 # ECS SG
 resource "aws_security_group" "ecs_sg" {
-    name        = "${var.app_name}-${var.environment}-ecs-sg"
-    description = "Allow traffic from ALB to ECS tasks"
-    vpc_id      = data.aws_vpc.default.id
+  name        = "${var.app_name}-${var.environment}-ecs-sg"
+  description = "Allow traffic from ALB to ECS tasks"
+  vpc_id      = data.aws_vpc.default.id
 
-ingress {
-    description = "ECS task security group for Bat or Brolly"
-    from_port   = 8000
-    to_port     = 8000
-    protocol    = "tcp"
+  ingress {
+    description     = "ECS task security group for Bat or Brolly"
+    from_port       = 8000
+    to_port         = 8000
+    protocol        = "tcp"
     security_groups = [aws_security_group.alb_sg.id]
-    }
+  }
 
-    egress {
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    tags = {
-        Name        = "${var.app_name}-${var.environment}"
-        Environment = var.environment
-        ManagedBy   = "terraform"
-    }
+  tags = {
+    Name        = "${var.app_name}-${var.environment}"
+    Environment = var.environment
+    ManagedBy   = "terraform"
+  }
 }
 
 # ALB
 
 resource "aws_lb" "app_loadbalancer" {
-    name               = "${var.app_name}-${var.environment}-alb"
-    internal           = false
-    load_balancer_type = "application"
-    security_groups    = [aws_security_group.alb_sg.id]
-    subnets            = data.aws_subnets.default.ids
+  name               = "${var.app_name}-${var.environment}-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = data.aws_subnets.default.ids
 
-    tags = {
-        Name        = "${var.app_name}-${var.environment}"
-        Environment = var.environment
-        ManagedBy   = "terraform"
-    }
+  tags = {
+    Name        = "${var.app_name}-${var.environment}"
+    Environment = var.environment
+    ManagedBy   = "terraform"
+  }
 }
 
 # ALB Target Group
 resource "aws_lb_target_group" "alb_tg" {
-    name        = "${var.app_name}-${var.environment}-alb-tg"
-    # where to forward the ALB traffic onto
-    port        = 8000
-    protocol    = "HTTP"
-    vpc_id      = data.aws_vpc.default.id
-    target_type = "ip"
+  name = "${var.app_name}-${var.environment}-alb-tg"
+  # where to forward the ALB traffic onto
+  port        = 8000
+  protocol    = "HTTP"
+  vpc_id      = data.aws_vpc.default.id
+  target_type = "ip"
 
-    health_check {
-        path                = "/"
-        protocol            = "HTTP"
-        healthy_threshold   = 2
-        unhealthy_threshold = 3
-        interval            = 30
-    }
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    healthy_threshold   = 2
+    unhealthy_threshold = 3
+    interval            = 30
+  }
 
-    tags = {
-        Name        = "${var.app_name}-${var.environment}"
-        Environment = var.environment
-        ManagedBy   = "terraform"
-    }
+  tags = {
+    Name        = "${var.app_name}-${var.environment}"
+    Environment = var.environment
+    ManagedBy   = "terraform"
+  }
 }
 
 # ALB listener
 resource "aws_lb_listener" "alb_listener" {
-    load_balancer_arn = aws_lb.app_loadbalancer.arn
-    port              = 80
-    protocol          = "HTTP"
+  load_balancer_arn = aws_lb.app_loadbalancer.arn
+  port              = 80
+  protocol          = "HTTP"
 
-    default_action {
-        type             = "forward"
-        target_group_arn = aws_lb_target_group.alb_tg.arn
-    }
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb_tg.arn
+  }
 
-    tags = {
-        Name        = "${var.app_name}-${var.environment}"
-        Environment = var.environment
-        ManagedBy   = "terraform"
-    }
+  tags = {
+    Name        = "${var.app_name}-${var.environment}"
+    Environment = var.environment
+    ManagedBy   = "terraform"
+  }
 }
 
 # ECS
 # Cluster
 resource "aws_ecs_cluster" "ecs_cluster" {
-    name = "${var.app_name}-${var.environment}-ecs-cluster"
+  name = "${var.app_name}-${var.environment}-ecs-cluster"
 
-    tags = {
-        Name        = "${var.app_name}-${var.environment}"
-        Environment = var.environment
-        ManagedBy   = "terraform"
-    }
+  tags = {
+    Name        = "${var.app_name}-${var.environment}"
+    Environment = var.environment
+    ManagedBy   = "terraform"
+  }
 }
 
 # Task Definition
 resource "aws_ecs_task_definition" "ecs_task" {
-    family                   = "${var.app_name}-${var.environment}"
+  family = "${var.app_name}-${var.environment}"
 
-            requires_compatibilities = ["FARGATE"]
-            network_mode             = "awsvpc"
-            cpu                      = var.cpu
-            memory                   = var.memory
-            execution_role_arn       = aws_iam_role.role.arn
-    container_definitions = jsonencode([
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = var.cpu
+  memory                   = var.memory
+  execution_role_arn       = aws_iam_role.role.arn
+  container_definitions = jsonencode([
+    {
+      name      = aws_ecr_repository.app_container.name
+      image     = var.ecr_uri
+      essential = true
+      portMappings = [
         {
-            name      = aws_ecr_repository.app_container.name
-            image     = var.ecr_uri
-            essential = true
-            portMappings = [
-                {
-                    containerPort = var.container_port
-                    protocol      = "tcp"
-                }
-            ]
-
-            logConfiguration = {
-                logDriver = "awslogs"
-                options = {
-                    awslogs-group         = aws_cloudwatch_log_group.logs.name
-                    awslogs-region        = var.region
-                    awslogs-stream-prefix = "ecs"
-                }
-            }
+          containerPort = var.container_port
+          protocol      = "tcp"
         }
-    ])
+      ]
 
-    tags = {
-        Name        = "${var.app_name}-${var.environment}"
-        Environment = var.environment
-        ManagedBy   = "terraform"
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-group         = aws_cloudwatch_log_group.logs.name
+          awslogs-region        = var.region
+          awslogs-stream-prefix = "ecs"
+        }
+      }
     }
+  ])
+
+  tags = {
+    Name        = "${var.app_name}-${var.environment}"
+    Environment = var.environment
+    ManagedBy   = "terraform"
+  }
 }
 
 # ECS Service
 resource "aws_ecs_service" "ecs_service" {
-    name            = "${var.app_name}-${var.environment}-service"
-    cluster         = aws_ecs_cluster.ecs_cluster.id
-    task_definition = aws_ecs_task_definition.ecs_task.arn
-    desired_count   = var.desired_task_count
-    launch_type     = "FARGATE"
+  name            = "${var.app_name}-${var.environment}-service"
+  cluster         = aws_ecs_cluster.ecs_cluster.id
+  task_definition = aws_ecs_task_definition.ecs_task.arn
+  desired_count   = var.desired_task_count
+  launch_type     = "FARGATE"
 
-    network_configuration {
-        subnets          = data.aws_subnets.default.ids
-        security_groups  = [aws_security_group.ecs_sg.id]
-        assign_public_ip = true
-    }
+  network_configuration {
+    subnets          = data.aws_subnets.default.ids
+    security_groups  = [aws_security_group.ecs_sg.id]
+    assign_public_ip = true
+  }
 
-    load_balancer {
-        target_group_arn = aws_lb_target_group.alb_tg.arn
-        container_name   = aws_ecr_repository.app_container.name
-        container_port   = var.container_port
-    }
+  load_balancer {
+    target_group_arn = aws_lb_target_group.alb_tg.arn
+    container_name   = aws_ecr_repository.app_container.name
+    container_port   = var.container_port
+  }
 
-    tags = {
-        Name        = "${var.app_name}-${var.environment}"
-        Environment = var.environment
-        ManagedBy   = "terraform"
-    }
+  tags = {
+    Name        = "${var.app_name}-${var.environment}"
+    Environment = var.environment
+    ManagedBy   = "terraform"
+  }
 }
 
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,54 +1,54 @@
 # infra variables for AWS
 
 variable "region" {
-    description = "region for aws resources"
-    type = string
-    default = "eu-west-2"
+  description = "region for aws resources"
+  type        = string
+  default     = "eu-west-2"
 }
 
 variable "account_id" {
   description = "aws account id"
-  type = string
+  type        = string
 }
 
 variable "ecr_uri" {
   description = "URI for ECR image"
-  type = string
+  type        = string
 }
 
 variable "app_name" {
   description = "bat or brolly app name"
-  type = string
-  default = "bat-or-brolly"
+  type        = string
+  default     = "bat-or-brolly"
 }
 
 # environment can be dev, staging or prod - dev is always default first
 variable "environment" {
   description = "environment for code"
-  type = string
-  default = "dev"
+  type        = string
+  default     = "dev"
 }
 
 variable "container_port" {
   description = "port to connect to container"
-  type = number
-  default = 8000
+  type        = number
+  default     = 8000
 }
 
 variable "desired_task_count" {
   description = "desired number of tasks running"
-  type = number
-  default = 1
+  type        = number
+  default     = 1
 }
 
 variable "cpu" {
   description = "cpu for each task"
-  type = number
-  default = 256
+  type        = number
+  default     = 256
 }
 
 variable "memory" {
   description = "memory allocated to each task"
-  type = number
-  default = 512
+  type        = number
+  default     = 512
 }
